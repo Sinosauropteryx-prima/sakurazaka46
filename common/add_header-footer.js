846c3adfb41f5c134c3c91aefc8d85570b66507e
Sinosauropteryx-prima <baremiannki500@icloud.com>
2025-03-11 19:46:16 +0900

    change

---
+++ common/add_header-footer.js
@@ -0,0 +1,7 @@
+fetch("header.html")
+    .then((response) => response.text())
+    .then((data) => document.querySelector(".body").insertAdjacentHTML('afterbegin', data));
+
+fetch("footer.html")
+    .then((response) => response.text())
+    .then((data) => document.querySelector(".body").insertAdjacentHTML('beforeend', data));

30ba33dea2164c7723d2780670598dad28de42a5
Sinosauropteryx-prima <baremiannki500@icloud.com>
2025-03-23 14:05:44 +0900

    update header

--- common/add_header-footer.js
+++ common/add_header-footer.js
@@ -1,7 +1,7 @@
 fetch("https://sinosauropteryx-prima.github.io/sakurazaka46/common/header.html")
     .then((response) => response.text())
-    .then((data) => document.querySelector(".body").insertAdjacentHTML('afterbegin', data));
+    .then((data) => document.querySelector("body").insertAdjacentHTML('afterbegin', data));
 
 fetch("https://sinosauropteryx-prima.github.io/sakurazaka46/common/footer.html")
     .then((response) => response.text())
-    .then((data) => document.querySelector(".body").insertAdjacentHTML('beforeend', data));
+    .then((data) => document.querySelector("body").insertAdjacentHTML('beforeend', data));
